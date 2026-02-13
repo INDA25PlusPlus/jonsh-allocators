93990d037956e038a36371ef733be8f632370422
Jonathan Hahne <jonathanhahne@c-83-226-241-243.bbcust.telenor.se>
2026-02-13 01:37:57 +0100

    feat: implemented linear allocator

--- src/linearallocator.zig
+++ src/linearallocator.zig
@@ -3,4 +3,52 @@ const std = @import("std");
 const LinearAllocator = struct {
     buffer: []u8,
     offset: usize,
+
+    pub fn init(buffer: []u8) LinearAllocator {
+        return .{
+            .buffer = buffer,
+            .offset = 0,
+        };
+    }
+
+    fn alloc(ctx: *anyopaque, len: usize, alignn: u8, return_address: usize) ?[*]u8 {
+        _ = return_address;
+        const self: *LinearAllocator = @ptrCast(@alignCast(ctx));
+
+        const align_offset = std.mem.alignForward(self.offset, alignn);
+
+        if (align_offset + len > self.buffer.len) {
+            return null;
+        }
+
+        const ptr = self.buffer.ptr + align_offset;
+        self.offset = align_offset + len;
+
+        return ptr;
+    }
+
+    fn resize(ctx: *anyopaque, buf: []u8, alignn: u8, new_len: usize, return_address: usize) bool {
+        _ = ctx;
+        _ = buf;
+        _ = alignn;
+        _ = new_len;
+        _ = return_address;
+        return false;
+    }
+
+    fn free(ctx: *anyopaque, buf: []u8, alignn: u8, return_address: usize) void {
+        _ = ctx;
+        _ = buf;
+        _ = alignn;
+        _ = return_address;
+    }
+
+    pub fn allocator(self: *LinearAllocator) std.mem.Allocator {
+        return std.mem.Allocator{
+            .ptr = self,
+            .alloc = alloc,
+            .resize = resize,
+            .free = free,
+        };
+    }
 };
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,4 @@
 const std = @import("std");
 const jonsh_allocators = @import("jonsh_allocators");
 
-pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try jonsh_allocators.bufferedPrint();
-}
+pub fn main() !void {}
